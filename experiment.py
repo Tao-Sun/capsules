Corrected the implementation of softmax function.

experiment.py
@@ -267,8 +267,8 @@ def eval_experiment(session, result, writer, last_step, max_steps, **kwargs):
         # print("predictions[i] shape" + str(predictions[i].shape))
         target_0 = batch_targets[i].flatten()
         prediction_0 = batch_predictions[i].flatten()
-        print(target_0[1:100])
-        print(prediction_0[1:100])
+        # print(target_0[1:100])
+        # print(prediction_0[1:100])
         intersection_0 = np.sum(np.multiply(target_0, prediction_0))
         batch_intersection_0 += intersection_0
         union_0 = np.sum(target_0) + np.sum(prediction_0)
@@ -276,9 +276,9 @@ def eval_experiment(session, result, writer, last_step, max_steps, **kwargs):
 
 
         target_1 = 1 - target_0
-        print(target_1[1:100])
+        # print(target_1[1:100])
         prediction_1 = 1 - prediction_0
-        print(prediction_1[1:100])
+        # print(prediction_1[1:100])
         intersection_1 = np.sum(np.multiply(target_1, prediction_1))
         batch_intersection_1 += intersection_1
         # print("positive_target_indices shape:" + str(positive_target_indices))

input_data/hippo/hippo_input.py
@@ -80,8 +80,8 @@ def read_and_decode(filename_queue, image_height, image_width):
     # Convert from [0, 255] -> [-0.5, 0.5] floats.
     image = tf.cast(image, tf.float32) * (1. / 255)
     label = tf.cast(label, tf.float32) * (1. / 255)
-    recons_label = features['label']
-    spare_label = tf.constant(1, tf.int64)
+    recons_label = tf.constant(1, tf.int64)
+    spare_label = tf.constant(0, tf.int64)
 
     return image, label, recons_label, spare_label
 

models/capsule_model.py
@@ -133,18 +133,22 @@ class CapsuleModel(model.Model):
     Returns:
       A list of network remakes of the targets.
     """
+
     num_pixels = features['depth'] * features['height'] * features['width']
-    remakes = []
+    fc_remakes = []
     targets = [(features['recons_label'], features['recons_image'])]
     if features['num_targets'] == 2:
       targets.append((features['spare_label'], features['spare_image']))
+    assert(features['recons_label'].get_shape() == features['spare_label'].get_shape())
 
     balance_factor = 1.0
 
     with tf.name_scope('recons'):
       for i in range(features['num_targets']):
         label, image = targets[i]
-        remakes.append(
+        print('label shape:' + str(label.get_shape()))
+        print('capsule_embedding shape:' + str(capsule_embedding.get_shape()))
+        fc_remakes.append(
             layers.fc_recons(
                 number=i,
                 capsule_mask=tf.one_hot(label, features['num_classes']),
@@ -152,22 +156,27 @@ class CapsuleModel(model.Model):
                 capsule_embedding=capsule_embedding,
                 layer_sizes=[1680],
                 num_pixels=num_pixels,
-                reuse=False, #(i > 0),
+                reuse=(i > 0),
                 recons_image=features['recons_image'],
                 num_targets=features['num_targets'],
                 balance_factor=balance_factor))
 
-    fc_2d_stack = tf.stack(remakes, axis=1)
+    fc_2d_stack = tf.stack(fc_remakes, axis=1)
     fc_2d = tf.reshape(fc_2d_stack, [-1, features['num_targets'] * 20, 6, 14])
-    label_logits = layers.deconv(fc_2d, False)
-    label_2d = tf.argmax(tf.nn.softmax(label_logits), 1)
+    print('fc_remakes[0] shape:' + str(fc_remakes[0].get_shape()))
+    label_logits = tf.transpose(layers.deconv(fc_2d, False), perm=[0, 2, 3, 1])
+    label_2d = tf.argmax(label_logits, axis=3)
 
     with tf.name_scope('loss'):
         image_2d = tf.cast(features['recons_image'], tf.int32)
-        logits_2d = tf.transpose(label_logits, perm=[0, 2, 3, 1])
+        #image_2d = tf.contrib.layers.flatten(features['recons_image'])
+        #_, recons_logits = tf.split(label_prob, [1, 1], axis=1)
+        #logits_2d = tf.contrib.layers.flatten(recons_logits)
 
         cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=image_2d,
-                                                                       logits=logits_2d)
+                                                                       logits=label_logits)
+        #cross_entropy = tf.nn.sigmoid_cross_entropy_with_logits(labels=image_2d, logits=logits_2d)
+
         loss = tf.reduce_mean(cross_entropy)
         # batch_loss = tf.reduce_mean(loss)
         balanced_loss = balance_factor * loss
@@ -175,7 +184,7 @@ class CapsuleModel(model.Model):
         tf.summary.scalar('reconstruction_error', balanced_loss)
 
     if self._hparams.verbose:
-      self._summarize_remakes(features, remakes)
+      self._summarize_remakes(features, fc_remakes)
 
     return label_2d
 

models/layers/layers.py
@@ -445,17 +445,18 @@ def fc_recons(number, capsule_mask, num_atoms, capsule_embedding, layer_sizes,
     The reconstruction images of shape [batch_size, num_pixels].
   """
   first_layer_size = layer_sizes[0]
+  print('first_layer_size:' + str(first_layer_size))
   capsule_mask_3d = tf.expand_dims(capsule_mask, -1)
   atom_mask = tf.tile(capsule_mask_3d, [1, 1, num_atoms])
   filtered_embedding = capsule_embedding * atom_mask
   filtered_embedding_2d = tf.contrib.layers.flatten(filtered_embedding)
-  print(filtered_embedding_2d.get_shape())
+  print("filtered_embedding_2d shape: " + str(filtered_embedding_2d.get_shape()))
   fc_logits = tf.contrib.layers.stack(
       inputs=filtered_embedding_2d,
       layer=tf.contrib.layers.fully_connected,
       stack_args=[(first_layer_size, tf.nn.relu)],
       reuse=reuse,
-      scope='fc' + str(number),
+      scope='fc',
       weights_initializer=tf.truncated_normal_initializer(
           stddev=0.1, dtype=tf.float32),
       biases_initializer=tf.constant_initializer(0.1))
@@ -526,5 +527,6 @@ def deconv(fc_2d, reuse):
         weights_initializer=tf.truncated_normal_initializer(
             stddev=0.1, dtype=tf.float32)
     )
+    print('label_logits shape:' + str(label_logits.get_shape()))
 
     return label_logits